cf035f4a8cf42b3c2d9170925181fd2443ceab0c
Krendelev <krendelev@gmail.com>
2019-09-02 20:19:37 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Space Instagram
+
+Returns list of users qualified to enter your [Instagram](https://instagram.com) sweepstakes according to a set of predefined rules.
+
+### How to install
+
+Python3 should be already installed.
+Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Put your [Instagram](https://instagram.com) `login` and `password` into the `.env` file in the working directory like this:
+
+```bash
+INSTA_LOGIN=myaccount
+INSTA_PASS=mypassword
+```
+
+Run `main.py` with link to your post as an argument.
+
+```bash
+$ python main.py https://www.instagram.com/p/B15-qRooKYe/
+['99marvik99', '_sadseal__', 'peryzt', 'yarik24_mr', 'tiiny.yy', 'iiamgerman_', '06.05m']
+```
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
---
+++ main.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+import re
+import instabot
+from dotenv import load_dotenv
+
+
+def get_mentions(comment):
+    pattern = re.compile(
+        r"@([A-Za-z0-9_.](?:(?:[A-Za-z0-9_.]|(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_.]))?)"
+    )
+    return pattern.findall(comment)
+
+
+def is_user_exists(bot, user):
+    user_id = bot.get_user_id_from_username(user)
+    return True if user_id else False
+
+
+def get_link():
+    parser = argparse.ArgumentParser(
+        description="Returns users qualified to enter sweepstakes"
+    )
+    parser.add_argument("URL", help="URL for your Instagram post")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    bot = instabot.Bot()
+    bot.login(username=os.environ["INSTA_LOGIN"], password=os.environ["INSTA_PASS"])
+
+    media_link = get_link()
+    media_id = bot.get_media_id_from_link(media_link.URL)
+    likers = bot.get_media_likers(media_id)
+    author = bot.get_media_info(media_id)[0]["user"]["username"]
+    followers = bot.get_user_followers(author)
+    comments = bot.get_media_comments_all(media_id)
+
+    qualified_users = []
+    for comment in comments:
+        user_id = str(comment["user"]["pk"])
+        username = comment["user"]["username"]
+        if username in qualified_users:
+            continue
+        mentions = get_mentions(comment["text"])
+        mention_verified = any(
+            is_user_exists(bot, mention) for mention in mentions if mentions
+        )
+        if mention_verified and user_id in likers and user_id in followers:
+            qualified_users.append(username)
+
+    print(qualified_users)
